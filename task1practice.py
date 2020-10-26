3bedbfd541a2cd4bb4ef6a43d89d519f6e991f95
Claire Laptop <clairenolan@hotmail.com>
2020-10-26 17:48:41 +0000

    26Oct2020 - formatted notebook entries; included references; identified two algorithms - both work; found function to give 100 decimal places although not accurate

---
+++ task1practice.py
@@ -0,0 +1,9 @@
+import math   # This will import math module
+
+#print ("Square root of 2 is : ", round(math.sqrt(2), 20))
+
+#print ("Square root of 2 is : ", format(math.sqrt(2), '.12g'))
+
+print ("Square root of 2 is : ", format(math.sqrt(2), '.100f'))
+
+#print ("Square root of 2 is : ", repr(math.sqrt(2)))
